312daad59219b1ae801142f69d56af12c0a063e3
BogdanSch <bogmira777@gmail.com>
2022-06-08 21:41:57 +0200

    Added working unfinished play buttons

--- js/script.js
+++ js/script.js
@@ -1,18 +1,24 @@
-function notAvailable(){
-    const links = document.querySelectorAll("a.unfinished");
-    links.forEach(
-        (element, key) =>{
-            element.outerHTML = 'Not available';
-        }
-    )
-}
+const allLinks = document.querySelectorAll("button.unfinished");
 
-const allLinks = document.querySelectorAll("a.unfinished");
 
 allLinks.forEach(
     (element, key) =>{
-        element.addEventListener("onClick", (e) =>{
-            notAvailable();
+        element.addEventListener("click", (e) =>{
+            console.log(element);
+            element.innerHTML = 'Not available';
+            element.style.cssText = 'background-color: rgb(255, 0, 0); border-radius: 10px;'
         });
     }
-)
+);
+
+// document.body.addEventListener('click', (e) => {
+//     const subList = document.querySelector('.header__lower__list');
+//     subList.style.cssText = 'display: none;';
+// });
+
+// const buttonSubList = document.querySelector('.sub-games') 
+
+// buttonSubList.addEventListener('click', (e) => {
+//     const subList = document.querySelector('.header__lower__list');
+//     subList.style.cssText = 'display: block;';
+// });
